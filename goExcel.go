3b06613e2d969224e1837fb0b93f2ee3ee06d19e
linzhaopeng <linzhaopn@gmail.com>
2020-06-05 17:50:09 +0800

    optimize

--- goExcel.go
+++ goExcel.go
@@ -8,28 +8,30 @@ import (
 
 const A = 65
 
-func Export(dataList []map[string]string, headList map[string]int, fileName string) {
+func Export(dataList []map[string]string, headList []string, fileName string) {
 	f, newHeadList := excelize.NewFile(), make(map[string]string)
 
-	// 设置表头
-	for k, v := range headList{
-		row := string(A + v)
-		if v > 25 {
-			if v < 52 {
-				row = "A" + string(A + v - 26)
-			} else {
-				row = "B" + string(A + v - 52)
-			}
+	// set header columns
+	// use uppercase from A to Z
+	// if run out of alphabet,rearrange from A to Z and join another alphabet also from A to Z after it
+	for k, v := range headList {
+		row := string(A + k)
+		if k > 25 {
+			// I think 27*26 columns is enough
+			times, offset := k / 26, k % 26
+			row = string(A+times-1) + string(A+offset-1)
 		}
 
-		f.SetCellValue("Sheet1", row + "1", k)
-		newHeadList[k] = row
+		// the header column is set on row 1st
+		f.SetCellValue("Sheet1", row+"1", k)
+		newHeadList[v] = row
 	}
 
-	for dataIndex, data := range dataList{
+	for dataIndex, data := range dataList {
+		// because the header column is set on row 1st,the data's row is from 2nd
 		row := strconv.Itoa(dataIndex + 2)
-		for k, v := range data{
-			f.SetCellValue("Sheet1", newHeadList[k] + row, v);
+		for k, v := range data {
+			f.SetCellValue("Sheet1", newHeadList[k]+row, v)
 		}
 	}
 
